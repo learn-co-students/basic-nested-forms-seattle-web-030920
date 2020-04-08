4f58167a2ba8694d539db06db606cbe18c19e93e
jeff_simon <jeffsbillinfo@gmail.com>
2020-04-07 18:30:49 -0700

    Done.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,10 +1,17 @@
 class PeopleController < ApplicationController
   def new
     @person = Person.new
+    @person.addresses.build(address_type: 'work')
+    @person.addresses.build(address_type: 'home')
   end
 
   def create    
-    Person.create(person_params)
+    person = Person.create(person_params)
+    if(person.valid?)
+      byebug
+    else
+      byebug
+    end
     redirect_to people_path
   end
 
@@ -15,6 +22,16 @@ class PeopleController < ApplicationController
   private
 
   def person_params
-    params.require(:person).permit(:name)
+    params.require(:person).permit(
+      :name,
+      addresses_attributes: [
+        :street_address_1,
+        :street_address_2,
+        :city,
+        :state,
+        :zipcode,
+        :adress_type
+      ]
+      )
   end
 end
